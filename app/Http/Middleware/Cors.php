0c8ea428e997219b72c3bed50fc8bf89ee1f401b
Arun <arun810780030@gmail.com>
2023-12-24 02:46:29 -0800

    initial commit

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next)
+    {
+        Log::info('CORS Middleware Executed');
+
+        // Headers set karne ka code yahaan rakhein
+        return $next($request) 
+            ->header('Access-Control-Allow-Origin', 'http://127.0.0.1:8000')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+    
+
+
+}
